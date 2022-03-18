2a57ce74adbdd90cf18da86b183c6cc7052e7477
HappySonia <soniahuyan@gmail.com>
2022-03-18 13:56:42 +1000

    before deploy change

--- db/db.js
+++ db/db.js
@@ -1,7 +1,18 @@
 const pg = require('pg')
 
-const db = new pg.Pool({
+let db;
+if (process.env.NODE_ENV === 'production') {
+  db = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
+} else {
+  db = new pg.Pool({
     database: 'slimy_db'
-})
+   
+  })
+}
 
 module.exports = db
