9233499dfb5e331f870f79879c9aa8ec7db08288
oscar <opawong@yahoo.com>
2022-03-23 17:26:57 +1100

    db update

--- db/db.js
+++ db/db.js
@@ -1,21 +1,6 @@
 const pg = require("pg");
 
 let db;
-if (process.env.NODE_ENV === 'production') {
-  db = new pg.Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  })
-} else {
-  db = new pg.Pool({
-    database: 'slimy_db'
-   
-  })
-}
-
-let db;
 if (process.env.NODE_ENV === "production") {
   db = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
